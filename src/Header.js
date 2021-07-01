ffc9ffaae497a9c366255bf0d7c674de79d08d44
DJ Drakos <dianajodrakos@gmail.com>
2021-06-30 17:55:03 -0700

    router pages up

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                <Link to="/">Home</Link> | 
+                {/* conditionally show this link based on token?*/}
+                <Link to="/todos">To Dos</Link> | 
+                <Link to="/signup">Sign Up</Link> | 
+                <Link to="/login">Login</Link>
+            </div>
+        )
+    }
+}
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                Y O U   A R E   H O M E
+            </div>
+        )
+    }
+}
